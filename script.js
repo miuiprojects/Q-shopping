a16f75a5ddddbba95bd6a45af9fa00d3331fe3a7
Madhavan Mi <80858743+miuiprojects@users.noreply.github.com>
2024-07-19 09:08:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 100) {
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
